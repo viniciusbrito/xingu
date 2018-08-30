a01675da3e10ee499f2603b1a3117ce4c89dbba8
Vinícius Brito <vinicius.fernandes.brito@gmail.com>
2018-08-30 12:03:37 -0300

    Adjustes on Layout: banner, add video

--- wp-content/themes/xingu/front-page.php
+++ wp-content/themes/xingu/front-page.php
@@ -7,53 +7,62 @@
 <main>
     
     <section class="header">        
-        <div class="container-fluid" id="header-slider" data-slides='["<?=$templateHome?>/assets/images/banner.jpg", "<?=$templateHome?>/assets/images/BG01-1440x900.jpg", "<?=$templateHome?>/assets/images/BG02-1440x900.jpg", "<?=$templateHome?>/assets/images/BG03-1440x900.jpg"]'>
-            <img style="display: none" src="<?=$templateHome?>/assets/images/BG01-1440x900.jpg" alt="">
-            <img style="display: none" src="<?=$templateHome?>/assets/images/BG02-1440x900.jpg" alt="">
-            <img style="display: none" src="<?=$templateHome?>/assets/images/BG03-1440x900.jpg" alt="">
-            <img style="display: none" src="<?=$templateHome?>/assets/images/banner.jpg" alt="">
-            
-            <!-- <div class="xingu-logo">
-                <img src="<?= $templateHome ?>/assets/images/logo_xingu.png" alt="Logo Xingu">
-            </div> -->
+        <div class="container-fluid" id="header-slider" data-slides='["<?=$templateHome?>/assets/images/banners/banner_1.jpg", "<?=$templateHome?>/assets/images/banners/banner_2.jpg", "<?=$templateHome?>/assets/images/banners/banner_3.jpg", "<?=$templateHome?>/assets/images/banners/banner_4.jpg", "<?=$templateHome?>/assets/images/banners/banner_5.jpg", "<?=$templateHome?>/assets/images/banners/banner_6.jpg", "<?=$templateHome?>/assets/images/banners/banner_7.jpg"]'>
+            <img style="display: none" src="<?=$templateHome?>/assets/images/banners/banner_1.jpg" alt="">
+            <img style="display: none" src="<?=$templateHome?>/assets/images/banners/banner_2.jpg" alt="">
+            <img style="display: none" src="<?=$templateHome?>/assets/images/banners/banner_3.jpg" alt="">
+            <img style="display: none" src="<?=$templateHome?>/assets/images/banners/banner_4.jpg" alt="">
+            <img style="display: none" src="<?=$templateHome?>/assets/images/banners/banner_5.jpg" alt="">
+            <img style="display: none" src="<?=$templateHome?>/assets/images/banners/banner_6.jpg" alt="">
+            <img style="display: none" src="<?=$templateHome?>/assets/images/banners/banner_7.jpg" alt="">
         </div>
         <div class="clearfix"></div>
         <div class="container-fluid header-grids">
             <div class="row">
                 <div class="col-md-12">
                     <div class="col-xs-2  col-sm-2 col-md-2 col-md-offset-1 header-grid">
-                        <div class="header-img text-center">
-                            <img src="<?=$templateHome?>/assets/images/icon4.png" class="img-responsive" alt="/">
-                            <h4>Infraestrutura</h4>
-                        </div>
+                        <a href="<?= get_permalink( get_page_by_title( 'Infraestrutura' ) ) ?>">
+                            <div class="header-img text-center">
+                                <img src="<?=$templateHome?>/assets/images/icon4.png" class="img-responsive" alt="/">
+                                <h4>Infraestrutura</h4>
+                            </div>
+                        </a>
                     </div>
     
                     <div class="col-xs-2  col-sm-2 col-md-2 header-grid">
-                        <div class="header-img text-center">
-                            <img src="<?=$templateHome?>/assets/images/icon5.png" class="img-responsive" alt="/">
-                            <h4>Consultoria</h4>
-                        </div>
+                        <a href="<?= get_permalink( get_page_by_title( 'Consultoria' ) ) ?>">
+                            <div class="header-img text-center">
+                                <img src="<?=$templateHome?>/assets/images/icon5.png" class="img-responsive" alt="/">
+                                <h4>Consultoria</h4>
+                            </div>
+                        </a>
                     </div>
     
                     <div class="col-xs-2  col-sm-2 col-md-2 header-grid">
-                        <div class="header-img text-center">
-                            <img src="<?=$templateHome?>/assets/images/icon6.png" class="img-responsive" alt="/">
-                            <h4>Pesquisa e Tecnologia</h4>
-                        </div>
+                        <a href="<?= get_permalink( get_page_by_title( 'Pesquisa e Tecnologia' ) ) ?>">
+                            <div class="header-img text-center">
+                                <img src="<?=$templateHome?>/assets/images/icon6.png" class="img-responsive" alt="/">
+                                <h4>Pesquisa e Tecnologia</h4>
+                            </div>
+                        </a>
                     </div>
     
                     <div class="col-xs-2  col-sm-2 col-md-2 header-grid">
-                        <div class="header-img text-center">
-                            <img src="<?=$templateHome?>/assets/images/icon7.png" class="img-responsive" alt="/">
-                            <h4>Estágio</h4>
-                        </div>
+                        <a href="<?= get_permalink( get_page_by_title( 'Estágio' ) ) ?>">
+                            <div class="header-img text-center">
+                                <img src="<?=$templateHome?>/assets/images/icon7.png" class="img-responsive" alt="/">
+                                <h4>Estágio</h4>
+                            </div>
+                        </a>
                     </div>
     
                     <div class="col-xs-2  col-sm-2 col-md-2 header-grid">
-                        <div class="header-img text-center">
-                            <img src="<?=$templateHome?>/assets/images/icon9.png" class="img-responsive" alt="/">
-                            <h4>Localização e contato</h4>
-                        </div>
+                        <a href="<?= get_permalink( get_page_by_title( 'Localização e Contato' ) ) ?>">
+                            <div class="header-img text-center">
+                                <img src="<?=$templateHome?>/assets/images/icon9.png" class="img-responsive" alt="/">
+                                <h4>Localização e contato</h4>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
@@ -70,10 +79,15 @@
                 <h1><?php the_title(); ?></h1>
                 <div class="row">
                     <div class="col-xs-12 col-md-6">
-                        <img src="<?php the_post_thumbnail_url(); ?>" alt="Sobre nós imagem" class="img-responsive">
+                        <div class="video">
+                            <video width="100%" height="auto" autoplay loop poster="<?=$templateHome?>/assets/images/preload.png">
+                                <source src="<?=$templateHome?>/assets/video/xingu.mp4" type="video/mp4">
+                                poster
+                            </video>
+                        </div>
                     </div>
                     <div class="col-xs-12 col-md-6">
-                        <p><?php the_excerpt(); ?></p>
+                        <p>A Xingu Pesquisa e Consultoria Agronômica é uma empresa independente de experimentação e consultoria agrícola, situada na cidade de Confresa – MT, com atuação na região Nordeste do estado de Mato Grosso (Vale Araguaia / Xingu) e região sul do estado do Pará. Em 2015, iniciou-se as atividades com operações voltadas a experimentação agrícola, com serviços de estudo e pesquisa, na inserção, adaptação e desenvolvimento de insumos (defensivos, fertilizantes e sementes), máquinas, implementos e equipamentos agropecuários. Em 2016, obteve-se o credenciamento junto ao Ministério da Agricultura, Pecuária e Abastecimento – MAPA, iniciando a implantação de trabalhos e a prestação de serviços de pesquisa independentes à indústria de proteção de culturas. Desde então, realizamos avaliações para empresas nacionais e internacionais, contribuindo para o sucesso do registro e lançamento futuro de novos ingredientes ativos no mercado brasileiro. De modo atender os produtores agrícolas através do desenvolvimento e implementação de novas estratégias na busca pelo melhor desempenho e performance, em 2017 trabalhando em estreita colaboração com nossos clientes, iniciou-se as atividades voltadas a consultoria agronômica, de modo a maximizar a relação entre a pesquisa e as unidades agrícolas. Oferecemos-lhes um pacote de atendimento profissional composto por planejamento agrícola efetivo, através de monitoramento frequente, voltado a rentabilidade final do setor produtivo de acordo com seus objetivos individuais.</p>
                         <a href="<?php the_permalink(); ?>" class="btn btn-sm btn-success">Ler mais</a>
                     </div>
                 </div>
@@ -98,7 +112,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-xs-12 col-md-4">
-                    <a href="" id="clima">
+                    <a href="<?= get_permalink( get_page_by_title( 'Clima' ) ) ?>" id="clima">
                         <div class="icone">
                             <span class="ico" id="clima-ico"></span>
                         </div>    
@@ -106,7 +120,7 @@
                     <h4>Clima</h4>
                 </div>
                 <div class="col-xs-12 col-md-4">
-                    <a href="" id="mercado">
+                    <a href="<?= get_permalink( get_page_by_title( 'Mercado' ) ) ?>" id="mercado">
                         <div class="icone">
                             <span class="ico" id="mercado-ico"></span>
                         </div>    
@@ -114,7 +128,7 @@
                     <h4>Mercado</h4>
                 </div>
                 <div class="col-xs-12 col-md-4">
-                    <a href="" id="agronegocio">
+                    <a href="<?= get_permalink( get_page_by_title( 'Agronegócio' ) ) ?>" id="agronegocio">
                         <div class="icone">
                             <span class="ico" id="agronegocio-ico"></span>
                         </div>    
@@ -122,7 +136,7 @@
                     <h4>Agronegócio</h4>
                 </div>
                 <div class="col-xs-12 col-md-4">
-                    <a href="" id="politica">
+                    <a href="<?= get_permalink( get_page_by_title( 'Política' ) ) ?>" id="politica">
                         <div class="icone">
                             <span class="ico" id="politica-ico"></span>
                         </div>    
@@ -130,7 +144,7 @@
                     <h4>Política</h4>
                 </div>
                 <div class="col-xs-12 col-md-4">
-                    <a href="" id="parceiros">
+                    <a href="<?= get_permalink( get_page_by_title( 'Parceiros' ) ) ?>" id="parceiros">
                         <div class="icone">
                             <span class="ico" id="parceiros-ico"></span>
                         </div>    
@@ -138,7 +152,7 @@
                     <h4>Parceiros</h4>
                 </div>
                 <div class="col-xs-12 col-md-4">
-                    <a href="" id="eventos">
+                    <a href="<?= get_permalink( get_page_by_title( 'Eventos' ) ) ?>" id="eventos">
                         <div class="icone">
                             <span class="ico" id="eventos-ico"></span>
                         </div>    
